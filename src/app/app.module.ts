5011d0c7271c300c5e20ce9e1f0a78503bce46d6
Kongaek <kongaek.seangpo@gmail.com>
2022-11-15 14:40:56 +0700

    15/11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteComponent } from './site/site.component';
+import { NavbarComponent } from './site/navbar/navbar.component';
+import { FooterComponent } from './site/footer/footer.component';
+import { LoginComponent } from './site/login/login.component';
+import { AdminComponent } from './site/admin/admin.component';
+import { SaleComponent } from './site/sale/sale.component';
+import { ManagerComponent } from './site/manager/manager.component';
+import { StoreComponent } from './site/store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SiteComponent,
+    NavbarComponent,
+    FooterComponent,
+    AdminComponent,
+    SaleComponent,
+    ManagerComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
